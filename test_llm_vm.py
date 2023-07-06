562fe5534c8693041a58debb6f0368c2f1b8544e
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2023-07-06 12:59:49 -0700

    removed open ai key

--- test_llm_vm.py
+++ test_llm_vm.py
@@ -1,6 +1,6 @@
 from llm_vm import llm_vm as l
 
-openai_key = "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"
+openai_key = ""
 ret = l.complete("Who are the top 10 greatest soccer players", "", openai_key)
 assert type(ret) == str
 ret = l.complete("Who are the top 10 greatest soccer players", "", "")
