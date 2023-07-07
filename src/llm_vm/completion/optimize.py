36a7b4ade2f5e73b98fc30c8b12c622a0ec2b728
vico <victorch9@gmail.com>
2023-07-07 13:04:34 +0300

    Handle token maxing and generation errors
    Added tiktoken to count generated tokens

--- src/llm_vm/completion/optimize.py
+++ src/llm_vm/completion/optimize.py
@@ -11,6 +11,7 @@ import abc
 import requests
 import hashlib
 import pickle
+import tiktoken
 
 try:
     import data_synthesis
@@ -110,7 +111,17 @@ def CALL_BIG(prompt, gpt4=False, **kwargs):
         model="gpt-3.5-turbo-0301" if not gpt4 else 'gpt-4',
         **kwargs)
 
-    return ans['choices'][0]['message']['content']
+    if ans["choices"][0]["finish_reason"] == "stop":
+        return ans["choices"][0]["message"]["content"]
+    elif ans["choices"][0]["finish_reason"] == "length":
+        enc = tiktoken.get_encoding("cl100k_base")
+        num_tokens = len(enc.encode(ans["choices"][0]["message"]["content"]))
+        print(f"Max tokens generated: {num_tokens} tokens", file=sys.stderr)
+        return ans["choices"][0]["message"]["content"]
+    elif ans["choices"][0]["finish_reason"] == "content_filter":
+        print(f"Error occurred while generating response. Your prompt was flagged by Open AI content moderation.", file=sys.stderr)
+    else:
+        print("Error occurred while generating response.", file=sys.stderr)
 
 def CALL_SMALL(*args, **kwargs):
     ans = openai.Completion.create(*args, **kwargs)
