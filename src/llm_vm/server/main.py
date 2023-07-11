5bcdc53014622e20995428e2c0d045bc1d4072c7
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2023-07-11 13:44:19 -0400

    fixups

--- src/llm_vm/server/main.py
+++ src/llm_vm/server/main.py
@@ -1,3 +1,5 @@
+from llm_vm.server import app
+
 import flask
 from flask import request, jsonify
 import json
@@ -7,72 +9,31 @@ import importlib
 import openai
 import os
 import hashlib
-from llm_vm.completion.optimize import LocalOptimizer
-
 
 # from test_agent import run_test
 from flask_cors import CORS
-
 from contextlib import contextmanager
 
+import llm_vm.server.routes as routes 
+from llm_vm.completion.optimize import LocalOptimizer
+
 optimizer = LocalOptimizer(MIN_TRAIN_EXS=2,openai_key=None)
+# Flask Configuration
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''home'''
 
-@app.route('/v1/complete', methods=['POST']) 
-def optimizing_complete():
-    data = json.loads(request.data)
-    static_context = data["context"]
-    dynamic_prompt = data["prompt"]
-    data_synthesis = False
-    finetune = False
-    kwargs = {}
-    if "openai_key" not in data.keys():
-        return {"status":0, "resp":"No OpenAI key provided"}
-        
-    if "temperature" in data.keys():
-        if type(data["temperature"]) != float and type(data["temperature"]) != int:
-            return {"status":0, "resp":"Wrong Data Type for temperature"}
-        else:
-            kwargs.update({"temperature":data["temperature"]})
-    
-    if "data_synthesis" in data.keys():
-        if type(data["data_synthesis"])==bool:
-            data_synthesis = data["data_synthesis"]
-        else:
-            return {"status":0, "resp":"Wrong Data Type for data_synthesis"}
-    
-    if "finetune" in data.keys():
-        if type(data["finetune"])==bool:
-            finetune = data["finetune"]
-        else:
-            return {"status":0, "resp":"Wrong Data Type for finetune"}
-    
-   
-    try:
-        openai.api_key = data["openai_key"]
-        os.environ['OPENAI_API_KEY']=openai.api_key
-        print(os.getenv("OPENAI_API_KEY"))
-    except:
-        raise Exception("No OpenAI Key Provided")
-    
-    optimizer.openai_key = openai.api_key
-    try:
-        completion = optimizer.complete(static_context,dynamic_prompt,data_synthesis=data_synthesis,finetune = finetune, **kwargs)
-    except:
-        return {"status":0, "resp":"An error occured"}
-    
-    return {"completion":completion, "status": 200}
+app.register_blueprint(routes.bp)
 
 
+# we are gonna want 
 def main_server_entry_point():
     # make this more configurable soon
     app.run(host="192.168.1.75", port=3002)
 
-if __name__ == '__main__':
-    app.run(host="192.168.1.75", port=3002)
+
+# we can reenable this if needed, but really use the cli_entry-points in pyproject.toml please
+# if __name__ == '__main__':
+#     # app.run(host="192.168.1.75",port=3002) # run at specified IP
+#     main_server_entry_point() # for running local
