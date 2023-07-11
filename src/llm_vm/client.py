478d7f6aee11701234ba041a650d2a42ead45030
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2023-07-11 18:59:51 -0400

    simple fixups

--- src/llm_vm/client.py
+++ src/llm_vm/client.py
@@ -2,8 +2,8 @@
 import requests
 import json
 
-class llm_vm:
-    def complete(prompt, context, openai_key, finetune=False, data_synthesis = False, temperature=0):
+
+def complete(prompt, context, openai_key, finetune=False, data_synthesis = False, temperature=0):
         
         url = 'http://192.168.1.75:3002/v1/complete'
         params = {'prompt': prompt, "context":context, "finetune":finetune, "data_synthesis":data_synthesis, "openai_key":openai_key, "temperature":temperature}
