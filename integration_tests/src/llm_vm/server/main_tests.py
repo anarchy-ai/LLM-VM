f78551c6600a7842052ed73715581cfd14f3c3d3
Andrew Nelson <andrew@kali.daleego>
2023-07-12 18:38:05 -0400

    basic test to validate the stop token behavior for configed llms

--- integration_tests/src/llm_vm/server/main_tests.py
+++ integration_tests/src/llm_vm/server/main_tests.py
@@ -3,11 +3,28 @@ import llm_vm.client as l
 
 def basic_completion_test():
     data = l.complete(
-        prompt = 'what is the snuff?',
+        prompt = 'How do you make butter?',
         context = 'repeat the prompt 5 times',
-        openai_key = os.getenv("OPENAI_API_KEY"),)
-        # stoptoken= ['airplane','sniff', 'snafe', 'snoopy','sneepy'])
-    print(data)
+        openai_key = os.getenv("OPENAI_API_KEY"))
+    return data
+
+def stop_token_test():
+    data = l.complete(
+        prompt = 'How do you make butter?',
+        context = 'repeat the prompt 5 times',
+        openai_key = os.getenv("OPENAI_API_KEY"),
+        stoptoken = 'butter')
+    return data
+
+def stop_tokens_test():
+    data = l.complete(
+        prompt = 'How do you make butter?',
+        context = 'repeat the prompt 5 times',
+        openai_key = os.getenv("OPENAI_API_KEY"),
+        stoptoken = ['butter', 'batter', 'bitter', 'bread'])
+    return data
 
 if __name__ == '__main__':
-    basic_completion_test()
+    print(basic_completion_test())
+    print(stop_token_test())
+    print(stop_tokens_test())
