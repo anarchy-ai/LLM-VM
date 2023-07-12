7bcd40b5131e28b68b73c0156a74e46b39fd840e
Andrew Nelson <andrew@kali.daleego>
2023-07-12 09:39:38 -0400

    Removed unnecessary import

--- tests/src/llm_vm/server/main_tests.py
+++ tests/src/llm_vm/server/main_tests.py
@@ -1,11 +1,9 @@
-import requests
 import os
 import llm_vm.client as l
 
 if __name__ == '__main__':
     data = l.complete(
         prompt = 'what is the snuff?',
-        context = 'repeat the prompt 5 times: ',
-        openai_key = os.getenv("OPENAI_API_KEY"),
-        stoptoken = 'snuff')
+        context = 'repeat the prompt 5 times',
+        openai_key = os.getenv("OPENAI_API_KEY"))
     print(data)
