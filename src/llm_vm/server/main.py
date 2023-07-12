8934125e9cb519b857e1e9ae9ff89f8ff55e5610
Andrew Nelson <andrew@kali.daleego>
2023-07-12 08:41:11 -0400

    Added cli network specification to main.py in the server

--- src/llm_vm/server/main.py
+++ src/llm_vm/server/main.py
@@ -7,6 +7,7 @@ import importlib
 import openai
 import os
 import hashlib
+import sys
 
 from flask_cors import CORS
 from contextlib import contextmanager
@@ -21,6 +22,30 @@ app.config["DEBUG"] = True
 
 app.register_blueprint(routes.bp)
 
-def main_server_entry_point():
+def main_server_entry_point(host = '127.0.0.1', port = 3002):
     # make this more configurable soon
-    app.run(host="192.168.1.75", port=3002)
+    app.run(host = host,port = port)
+
+def cli():
+    if len(sys.argv) > 1:
+        try:
+            # We're going to except on a value error at the int exchange
+            # If the cli argument isn't an integer then we'll try it as an address
+            port = int(sys.argv[1])
+            host = sys.argv[2] if len(sys.argv) > 2 else '127.0.0.1'
+        except:
+            # if it doesn't fit the above then we're likely looking at an addres
+            host = sys.argv[1]
+            # check if the port has been specified
+            port = int(sys.argv[2]) if len(sys.argv) > 2 and int(sys.argv[2]) < 65536 else 3002
+        if port > 66535:
+            print('Port defined out of range, defaulting to 3002')
+            port = 3002
+        main_server_entry_point(host = host, port = port)
+    else:
+        main_server_entry_point()
+
+    
+if __name__ == '__main__':
+    cli()
+
