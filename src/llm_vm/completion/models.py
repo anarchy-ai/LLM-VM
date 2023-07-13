5aa0fafdee776a5320026d85d831d75e49107309
Andrew Nelson <andrew@kali.daleego>
2023-07-13 09:52:38 -0400

    adjusted print statements for user clarity when spinning up server

--- src/llm_vm/completion/models.py
+++ src/llm_vm/completion/models.py
@@ -3,8 +3,8 @@ from llm_vm.server.anarchparse import args
 import json
 import os
 
-print(args.big_model)
-print(args.small_model)
+print('Big LLM Model: ' + args.big_model)
+print('Small LLM Model: ' + args.small_model)
 
 MODEL_DICT = {
     "opt":llms.Small_Local_OPT,
@@ -17,8 +17,6 @@ MODEL_DICT = {
 
 class ModelConfig:
     def __init__(self):
-        f = open('src/llm_vm/completion/config.json')
-        data = json.load(f)
         self.big_model = MODEL_DICT[args.big_model]()
         self.small_model = MODEL_DICT[args.small_model]()
 
