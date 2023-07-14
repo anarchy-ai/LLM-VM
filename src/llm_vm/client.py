594a00ff4f6a6f39e2fe1483583440cff5ca9b28
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2023-07-14 12:22:21 -0700

    added programmatic intereface in client

--- src/llm_vm/client.py
+++ src/llm_vm/client.py
@@ -1,7 +1,10 @@
 
-import requests
-import json
+import openai
+from llm_vm.agents.REBEL import agent
+from llm_vm.completion.optimize import LocalOptimizer
 
+# load optimizer for endpoint use
+optimizer = LocalOptimizer(MIN_TRAIN_EXS=2,openai_key=None)
 
 def complete(prompt,
              context,
@@ -9,24 +12,48 @@ def complete(prompt,
              finetune=False,
              data_synthesis = False,
              temperature=0,
-             stoptoken = None,):
+             stoptoken = None,
+             tools = None):
     
-    url = 'http://127.0.0.1:3002/v1/complete'
-    params = {
-        'prompt': prompt,
-        "context":context,
-        "finetune":finetune,
-        "data_synthesis":data_synthesis,
-        "openai_key":openai_key,
-        "temperature":temperature,
-        }
+    rebel_agent = agent.Agent("", [], verbose=1)
+    static_context = context
+    dynamic_prompt = prompt
+    use_rebel_agent = False
+    kwargs = {}
+    if openai_key is None:
+        return {"status":0, "resp":"No OpenAI key provided"}
+        
     
-    if stoptoken:
-        params["stoptoken"] = stoptoken
+    kwargs.update({"temperature":temperature})
+
+    if stoptoken is not None:
+        kwargs.update({"stop":stoptoken})                 
         
-    x = requests.post(url, json = params)
-    if json.loads(x.text)["status"] == 0:
-        return Exception(json.loads(x.text)["resp"])
-        
-    return json.loads(x.text)["completion"]
+    
+
+    
+    if tools is not None:
+        use_rebel_agent = True
+
+    try:
+        openai.api_key = openai_key
+    except:
+        return  {"status":0, "resp":"Issue with OpenAI key"}
+    
+    optimizer.openai_key = openai.api_key
+    agent.set_api_key(openai.api_key,"OPENAI_API_KEY")
+
+    try:
+        if not use_rebel_agent:
+            completion = optimizer.complete(static_context,dynamic_prompt,data_synthesis=data_synthesis,finetune = finetune, **kwargs)
+            
+        else:
+            completion = rebel_agent.run(static_context+dynamic_prompt,[])[0]
+    except Exception as e:
+        return {"status":0, "resp": str(e)}
+    
+    return {"completion":completion, "status": 200}
+
+
+    
     
