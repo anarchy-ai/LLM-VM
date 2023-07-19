316176adba38e77ead22df56fd1e5a002f944629
dorkitude <kyle@kylewild.com>
2023-07-19 14:23:16 -0700

    resolve merge conflicts

--- tests/test_data_synthesis.py
+++ tests/test_data_synthesis.py
@@ -11,32 +11,24 @@ if __name__ == "__main__":
         load_dotenv()
     except:
         pass
-<<<<<<< HEAD:src/llm_vm/completion/test_data_synthesis.py
     openai.api_key = os.getenv('LLM_VM_OPENAI_API_KEY')
     print("key:", openai.api_key)
-=======
->>>>>>> main:tests/test_data_synthesis.py
 
- 
+
     data_synthesizer = data_synthesis.DataSynthesis(0.87, 50)
-<<<<<<< HEAD:src/llm_vm/completion/test_data_synthesis.py
-    optimizer = LocalOptimizer(MIN_TRAIN_EXS=2)
 
-=======
-    
->>>>>>> main:tests/test_data_synthesis.py
     # for one-shot prompt
     prompt = "What is the currency in myanmmar?"
     response = client.complete(prompt=prompt, context = "", openai_key="",temperature = 0.0)["completion"]
     print(f"Prompt: {prompt} /nResponse: {response}")
-    
+
     # for k-shot prompt
     prompt_list = ["What is the currency in Madagascar?", "What is the currency in myanmmar?", "What is the currency in Morocco?"]
     response_list = []
     for p in prompt_list:
         res = client.complete(prompt=p, context = "", openai_key="", temperature = 0.0)
         response_list.append(res["completion"])
-    
+
 
 
 
