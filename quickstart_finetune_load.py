91317d88002ed7508ca24419f1b59fe21edb2053
abhigya sodani <abhigyasodani@ucla.edu>
2023-08-03 20:47:26 +0000

    added llama training

--- quickstart_finetune_load.py
+++ quickstart_finetune_load.py
@@ -3,14 +3,14 @@ from llm_vm.client import Client
 import os
 
 # Instantiate the client specifying which LLM you want to use
-client = Client(big_model='pythia')
+client = Client(big_model='llama')
 
 # specify the file name of the finetuned model to load
-model_name = '2023-08-03T01:05:05_pythia-70m-deduped.pt'
+model_name = '2023-08-03T20:29:27_open_llama_3b_v2.pt'
 client.load_finetune(model_name)
 
 # Put in your prompt and go!
 response = client.complete(prompt = 'Split Q into subquestions. Q: Which city is warmer beijing or shanghai?',
                            context='')
-#print(response)
+print(response)
 # Anarchy is a political system in which the state is abolished and the people are free...
