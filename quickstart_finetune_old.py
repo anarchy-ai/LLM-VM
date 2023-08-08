b339015e88f63e824d9e8336a96a91e48c7d2f70
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2023-08-08 13:29:59 -0700

    added testing framework

--- quickstart_finetune_old.py
+++ quickstart_finetune_old.py
@@ -9,7 +9,7 @@ client = Client(big_model='chat_gpt', small_model='llama')
 # Put in your prompt and go!
 response = client.complete(prompt = "Q: Locate and delete all emails that were sent to my manager last year.", 
                            context='Split the "Q" into its subtasks and return that as a list separated by commas. Return an empty string if no subtasks are necessary. \n\n Q: Find all the files in my system that were sent to HR before July 2nd. \n\n Find all files in system., Using previous answer search for files that were sent to HR., Using previous answer search for all files that were sent before July 2nd.<ENDOFLIST>',
-                           openai_key="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",
+                           openai_key="",
                            temperature=0.0,
                            data_synthesis=True,
                            finetune=True,)
