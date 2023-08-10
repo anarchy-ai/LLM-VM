0d6172132d6ede55474d36e8682627959b54d292
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2023-08-10 16:05:16 -0700

    added old file

---
+++ quickstart_finetune_load_old.py
@@ -0,0 +1,15 @@
+# import our client
+from llm_vm.client import Client
+import os
+
+# Instantiate the client specifying which LLM you want to use
+client = Client(big_model='pythia')
+
+# specify the file name of the finetuned model to load
+model_name =  '2023-08-08T22:44:34_pythia-70m-deduped.pt'
+client.load_finetune(model_name)
+
+# Put in your prompt and go!
+response = client.complete(prompt = 'Q: Considered the strongest recorded tropical cyclone, which cyclone had a film made about it in 2007?', context='')["completion"].split("<ENDOFLIST>")[0]
+print(response)
+# Anarchy is a political system in which the state is abolished and the people are free...
