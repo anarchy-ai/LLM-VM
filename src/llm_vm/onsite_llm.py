f361a8aaea8bf618af3cedbfdbf18b23a2e27a87
vico <victorch9@gmail.com>
2023-08-15 20:46:31 +0300

    removed regex to dfa function

--- src/llm_vm/onsite_llm.py
+++ src/llm_vm/onsite_llm.py
@@ -488,273 +488,6 @@ class TokenConstraint(ABC):
     def construct_filter_set(self, expression):
        pass
 
-    def _regex_to_dfa(self, r_string):
-        non_symbols = ['+', '*', '.', '(', ')', '?', '|']
-        nfa = {} 
-        dfa = {}
-        nfa_states = []
-        dfa_states = []
-
-        class charType:
-            SYMBOL = 1
-            CONCAT = 2
-            UNION  = 3
-            KLEENE = 4
-
-        class NFAState:
-            def __init__(self):
-                self.next_state = {}
-
-        class ExpressionTree:
-            def __init__(self, charType, value=None):
-                self.charType = charType
-                self.value = value
-                self.left = None
-                self.right = None
-            
-
-        def make_exp_tree(regexp):
-            stack = []
-            for c in regexp:
-                if c == "+":
-                    z = ExpressionTree(charType.UNION)
-                    z.right = stack.pop()
-                    z.left = stack.pop()
-                    stack.append(z)
-                elif c == "|":
-                    z = ExpressionTree(charType.UNION)
-                    z.right = stack.pop()
-                    z.left = stack.pop()
-                    stack.append(z)
-                elif c == ".":
-                    z = ExpressionTree(charType.CONCAT)
-                    z.right = stack.pop()
-                    z.left = stack.pop()
-                    stack.append(z)
-                elif c == "?":
-                    z = ExpressionTree(charType.KLEENE)
-                    z.left = stack.pop() 
-                    stack.append(z)
-                elif c == "*":
-                    z = ExpressionTree(charType.KLEENE)
-                    z.left = stack.pop() 
-                    stack.append(z)
-                elif c == "(" or c == ")":
-                    continue  
-                else:
-                    stack.append(ExpressionTree(charType.SYMBOL, c))
-            return stack[0]
-
-
-        def compPrecedence(a, b):
-            p = ["|", "." , "?", "+", "*"]
-            return p.index(a) > p.index(b)
-
-
-        # construct E-NFA
-        def compute_regex(exp_t):
-            if exp_t.charType == charType.CONCAT:
-                return do_concat(exp_t)
-            elif exp_t.charType == charType.UNION:
-                return do_union(exp_t)
-            elif exp_t.charType == charType.KLEENE:
-                return do_kleene_star(exp_t)
-            else:
-                return eval_symbol(exp_t)
-
-
-        def eval_symbol(exp_t):
-            start = NFAState()
-            end = NFAState()
-            
-            start.next_state[exp_t.value] = [end]
-            return start, end
-
-
-        def do_concat(exp_t):
-            left_nfa  = compute_regex(exp_t.left)
-            right_nfa = compute_regex(exp_t.right)
-
-            left_nfa[1].next_state['$'] = [right_nfa[0]]
-            return left_nfa[0], right_nfa[1]
-
-
-        def do_union(exp_t):
-            start = NFAState()
-            end = NFAState()
-
-            first_nfa = compute_regex(exp_t.left)
-            second_nfa = compute_regex(exp_t.right)
-
-            start.next_state['$'] = [first_nfa[0], second_nfa[0]]
-            first_nfa[1].next_state['$'] = [end]
-            second_nfa[1].next_state['$'] = [end]
-
-            return start, end
-
-
-        def do_kleene_star(exp_t):
-            start = NFAState()
-            end = NFAState()
-
-            starred_nfa = compute_regex(exp_t.left)
-
-            start.next_state['$'] = [starred_nfa[0], end]
-            starred_nfa[1].next_state['$'] = [starred_nfa[0], end]
-
-            return start, end
-
-        # construct NFA
-        def arrange_transitions(state, states_done, symbol_table):
-
-            if state in states_done:
-                return
-
-            states_done.append(state)
-
-            for symbol in list(state.next_state):
-                if symbol not in nfa['letters']:
-                    nfa['letters'].append(symbol)
-                for ns in state.next_state[symbol]:
-                    if ns not in symbol_table:
-                        symbol_table[ns] = sorted(symbol_table.values())[-1] + 1
-                        q_state = "Q" + str(symbol_table[ns])
-                        nfa['states'].append(q_state)
-                    nfa['transition_function'].append(["Q" + str(symbol_table[state]), symbol, "Q" + str(symbol_table[ns])])
-
-                for ns in state.next_state[symbol]:
-                    arrange_transitions(ns, states_done, symbol_table)
-
-
-        def final_st_dfs():
-            for st in nfa["states"]:
-                count = 0
-                for val in nfa['transition_function']:
-                    if val[0] == st and val[2] != st:
-                        count += 1
-                if count == 0 and st not in nfa["final_states"]:
-                    nfa["final_states"].append(st)
-
-
-        def arrange_nfa(fa):
-            nfa['states'] = []
-            nfa['letters'] = []
-            nfa['transition_function'] = []
-            nfa['start_states'] = []
-            nfa['final_states'] = []
-            q_1 = "Q" + str(1)
-            nfa['states'].append(q_1)
-            arrange_transitions(fa[0], [], {fa[0] : 1})
-            
-            nfa["start_states"].append("Q1")
-            final_st_dfs()
-
-        # parse regex
-        def add_concat(regex):
-            l = len(regex)
-            res = []
-            for i in range(l - 1):
-                res.append(regex[i])
-                if regex[i] not in non_symbols:
-                    if regex[i + 1] not in non_symbols or regex[i + 1] == '(':
-                        res += '.'
-                if regex[i] == ')' and regex[i + 1] == '(':
-                    res += '.'
-                if regex[i] == '*' and regex[i + 1] == '(':
-                    res += '.'
-                if regex[i] == '*' and regex[i + 1] not in non_symbols:
-                    res += '.'
-                if regex[i] == ')' and regex[i + 1] not in non_symbols:
-                    res += '.'
-
-            res += regex[l - 1]
-            return res
-
-        def compute_postfix(regexp):
-            stk = []
-            res = ""
-
-            for c in regexp:
-                if c not in non_symbols or c == "*":
-                    res += c
-                elif c == ")":
-                    while len(stk) > 0 and stk[-1] != "(":
-                        res += stk.pop()
-                    stk.pop()
-                elif c == "(":
-                    stk.append(c)
-                elif len(stk) == 0 or stk[-1] == "(" or compPrecedence(c, stk[-1]):
-                    stk.append(c)
-                else:
-                    while len(stk) > 0 and stk[-1] != "(" and not compPrecedence(c, stk[-1]):
-                        res += stk.pop()
-                    stk.append(c)
-
-            while len(stk) > 0:
-                res += stk.pop()
-
-            return res
-
-        def polish_regex(regex):
-            reg = add_concat(regex)
-            regg = compute_postfix(reg)
-            return regg
-        
-
-        pr = polish_regex(r_string)
-        et = make_exp_tree(pr)
-        enfa = compute_regex(et)
-        print("Constructing NFA...", flush=True)
-        arrange_nfa(enfa)
-
-        # construct DFA
-        def get_power_set(nfa_st):
-            print("Constructing DFA...", flush=True)
-            powerset = list(chain.from_iterable(combinations(nfa_st, r) for r in range(len(nfa_st)+1)))
-            return powerset
-        
-        dfa['states'] = []
-        dfa['letters'] = nfa['letters']
-        dfa['transition_function'] = []
-
-        for state in nfa['states']:
-            nfa_states.append(state)
-
-        dfa_states = get_power_set(nfa_states)
-
-        dfa['states'] = []
-        for states in dfa_states:
-            temp = []
-            for state in states:
-                temp.append(state)
-            dfa['states'].append(temp)
-
-        for states in dfa_states:
-            for letter in nfa['letters']:
-                q_to = []
-                for state in states:
-                    for val in nfa['transition_function']:
-                        start = val[0]
-                        inp = val[1]
-                        end = val[2]
-                        if state == start and letter == inp:
-                            if end not in q_to:
-                                q_to.append(end)
-                q_states = []
-                for i in states:
-                    q_states.append(i)
-                dfa['transition_function'].append([q_states, letter, q_to])
-
-        dfa['start_states'] = []
-        for state in nfa['start_states']:
-            dfa['start_states'].append([state])
-        dfa['final_states'] = []
-        for states in dfa['states']:
-            for state in states:
-                if state in nfa['final_states'] and states not in dfa['final_states']:
-                    dfa['final_states'].append(states)
-        
-        return dfa
     
 class RegexTokenConstraint(TokenConstraint):
     def construct_filter_set(self, expression):
@@ -920,16 +653,22 @@ class HFTransformersWithConstraints(HF_LLM):
         assert torch.is_tensor(kwargs["input_ids"]), "Input ids must be a torch tensor"
         
         if constraint_type == "regex":
+            if 'r_bias' not in kwargs:
+                r_bias = 10.0
+            else:
+                r_bias= float(kwargs['r_bias'])
             r_constraints = RegexTokenConstraint(self.model_identifier) 
             valid_tokens = r_constraints.construct_filter_set(kwargs['regex'])
             del kwargs['regex']
             seq_bias = {}
             for t in valid_tokens:
                 t_tuple = tuple(self.tokenizer.encode(t, add_special_tokens=False))
-                seq_bias[t_tuple] = 10.0
+                seq_bias[t_tuple] = r_bias
             logits_processor = LogitsProcessorList()
             logits_processor.append(RegexLogitsProcessor(seq_bias))
         elif constraint_type == "grammar":
+            if 'language' not in kwargs: 
+                raise Exception("language must be specified in kwargs")
             if kwargs['language'] == 'python':
                 python_constraint = PythonTokenConstraint(self.model_identifier)
                 terminals_map = python_constraint.parse_grammar()
@@ -942,6 +681,8 @@ class HFTransformersWithConstraints(HF_LLM):
                 logits_processor = LogitsProcessorList()
                 valid_tokens = set(valid_tokens)
                 del kwargs['language']
+            else: 
+                raise Exception(f"{kwargs['language']} is not supported")
         else:
             raise Exception(f"{constraint_type} not supported")
         
