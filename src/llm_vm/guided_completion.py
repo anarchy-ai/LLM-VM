3a12bde430aa54805c36da5cb068e317254461a5
vico <victorch9@gmail.com>
2023-08-25 17:46:10 +0300

    Added comments

--- src/llm_vm/guided_completion.py
+++ src/llm_vm/guided_completion.py
@@ -31,61 +31,83 @@ class TypeCompletion:
     
 class GrammarCompletion:
     def __init__(self, model_uri, tokenizer):
+        # Initialize the GrammarCompletion class with a model URI and tokenizer
         self.model_identifier = model_uri
         self.tokenizer = tokenizer
+
+        # Load model from HuggingFace
         self.model = AutoModelForCausalLM.from_pretrained(self.model_identifier)
         print(f"{self.model_identifier} model is ready for use on {self.model.device}", flush=True)
+        
+        # Define supported grammar types 
         self.supported_grammar = ["python", "json"]
+
+        # Initialize dict to store terminal symbols and their corresponding token mappings
         self.terminals_tokens_map = {}
 
     @property
     def eos_token_id(self):
         return self.model.config.eos_token_id
     
-    def complete(self, prompt, grammar_type='python'):
+    def complete(self, prompt, grammar_type='python', **model_kwargs):
+        # Check if the specified grammar type is supported
         if grammar_type.lower() not in self.supported_grammar:
             raise ValueError(f'{grammar_type} is not supported. The only valid grammar types are {self.supported_grammar}')
-        
+
+        # Encode the input prompt
         token_ids = self.tokenizer(prompt, return_tensors='pt')
         input_len = len(token_ids['input_ids'][-1])
 
         if grammar_type.lower() == 'python':
+            # For Python grammar, we create a PythonConstraint instance and parse the grammar
             python_constraint = PythonConstraint(self.model_identifier, self.tokenizer)
-            if(len(self.terminals_tokens_map) == 0):
-                terminal_regex_map = python_constraint.parse_grammar()
-                for k, v in terminal_regex_map.items():
-                    matching_tokens = python_constraint.construct_filter_set(v)
-                    self.terminals_tokens_map[k] = matching_tokens
-                self.terminals_tokens_map['$END'] = [(self.tokenizer.decode(self.eos_token_id), self.eos_token_id)]
-        elif grammar_type.lower() == 'json':
-            python_constraint = JSONConstraint(self.model_identifier, self.tokenizer)
-            if(len(self.terminals_tokens_map) == 0):
-                terminal_regex_map = python_constraint.parse_grammar()
-                for k, v in terminal_regex_map.items():
-                    matching_tokens = python_constraint.construct_filter_set(v)
-                    self.terminals_tokens_map[k] = matching_tokens
-                self.terminals_tokens_map['$END'] = [(self.tokenizer.decode(self.eos_token_id), self.eos_token_id)]
-            print('Created token map')
 
+            # Construct terminal tokens mapping if it's not yet initialized
+            if(len(self.terminals_tokens_map) == 0):
+                terminal_regex_map = python_constraint.parse_grammar()
+                for k, v in terminal_regex_map.items():
+                    matching_tokens = python_constraint.construct_filter_set(v)
+                    self.terminals_tokens_map[k] = matching_tokens
+                
+                # Add end-of-sequence token mapping
+                self.terminals_tokens_map['$END'] = [(self.tokenizer.decode(self.eos_token_id), self.eos_token_id)]
+
+            # Initialize the LogitsProcessorList for processing sequence logits and append GrammarLogitsProcessor instance
             logits_processor = LogitsProcessorList()
             logits_processor.append(GrammarLogitsProcessor(python_constraint, self.terminals_tokens_map))
+        elif grammar_type.lower() == 'json':
+            # For JSON grammar, we create a JSONConstraint instance and parse the grammar
+            json_constraint = JSONConstraint(self.model_identifier, self.tokenizer)
 
-        model_kwargs = {
-        'input_ids': token_ids['input_ids'],
-        'attention_mask': token_ids['attention_mask'],
-        'temperature': 1.0,
-        'max_new_tokens': 20,
-        }
+            # Construct terminal tokens mapping if it's not yet initialized
+            if(len(self.terminals_tokens_map) == 0):
+                terminal_regex_map = json_constraint.parse_grammar()
+                for k, v in terminal_regex_map.items():
+                    matching_tokens = json_constraint.construct_filter_set(v)
+                    self.terminals_tokens_map[k] = matching_tokens
+                
+                # Add end-of-sequence token mapping
+                self.terminals_tokens_map['$END'] = [(self.tokenizer.decode(self.eos_token_id), self.eos_token_id)]
+
+            # Initialize the LogitsProcessorList for processing sequence logits and append GrammarLogitsProcessor instance
+            logits_processor = LogitsProcessorList()
+            logits_processor.append(GrammarLogitsProcessor(json_constraint, self.terminals_tokens_map))
+
         model_kwargs["do_sample"] =  model_kwargs["temperature"] > 0
         model_kwargs["output_scores"] = True
         model_kwargs["return_dict_in_generate"] = True
         model_kwargs["logits_processor"] = logits_processor
-        res = self.model.generate(**model_kwargs, eos_token_id=self.eos_token_id, pad_token_id=self.eos_token_id)
-        res_text = self.tokenizer.batch_decode(res.sequences[:, input_len:], skip_special_tokens=True)[0]
+
+        # Generate completion using the model
+        res = self.model.generate(**model_kwargs, input_ids=token_ids['input_ids'], attention_mask=token_ids['attention_mask'], eos_token_id=self.eos_token_id, pad_token_id=self.eos_token_id)
+
+        # Decode completion and return the generated text, including input tokens
+        res_text = self.tokenizer.batch_decode(res.sequences, skip_special_tokens=True)[0]
         return res_text.strip()
     
 
 class GrammarConstraint(ABC):
+    # Initialize the GrammarConstraint class with the model URI and tokenizer
     def __init__(self, model_uri, tokenizer):
         self.model_uri = model_uri
         self.tokenizer = tokenizer
@@ -95,28 +117,33 @@ class GrammarConstraint(ABC):
     def construct_filter_set(self, expression):
         pass
 
+
 class PythonConstraint(GrammarConstraint): 
 
+    # Construct a filter set for valid tokens based on a given expression
     def construct_filter_set(self, expression):
         vocab = self.tokenizer.vocab
         valid_tokens = []
         specials = ['*', '+', ')']
+
+        # Preprocess expression to handle special cases
         if expression[0] in specials:
             expression = f'\{expression}'
         elif len(expression) == 1 and (expression == '[' or  expression == '('):
             expression = f'\{expression}'
         
         try:
+            # Compile the regex pattern and use it to match valid tokens in the vocabulary
             pattern = re.compile(expression, re.UNICODE)
             for token, id in vocab.items():
                 if pattern.match(token) is not None:
                    valid_tokens.append((token, id))
         except Exception as e: 
-            print(e, expression) 
+            print("Regex Compiling Error: ", f"{e} - {expression}") 
     
+        # Return a set of valid tokens based on the pattern
         return set(valid_tokens)
     
-
     def parse_grammar(self):
         # Create the Python parser with Lark, using the LALR algorithm
         self.parser = Lark.open_from_package('lark', 'python.lark', ['grammars'], parser='lalr', regex=True, lexer='contextual', postlex=PythonIndenter(), start='file_input')
@@ -124,42 +151,59 @@ class PythonConstraint(GrammarConstraint):
         t_map = {}
         for t in terminals:
             t_map[t.name] = t.pattern.value
+
+        # Return a map of terminal tokens and their corresponding regex patterns
         return t_map
 
-    def prefix_state(self, prefix_str):
+    def _prefix_state(self, prefix_str):
         valid_next = []
         try:      
+            # Parse the token sequence
             interactive_tree = self.parser.parse_interactive(prefix_str)
             interactive_tree.exhaust_lexer()
+            
+            # Get the valid next states
             valid_next = list(interactive_tree.accepts())
         except Exception as e:
-            print(e)
+            print("Parsing Error: ", e)
+        
+        # Return a list of valid next terminals
         return valid_next
 
     def construct_final_filter_set(self, prefix_ids, terminals_map):
+        # Decode the prefix IDs
         prefix_str = self.tokenizer.batch_decode(prefix_ids, skip_special_tokens=True)[0]
-        print(prefix_str)
-        next_lex = self.prefix_state(prefix_str)
+        
+        # Get valid next terminals for the prefix
+        next_lex = self._prefix_state(prefix_str)
         valid_next_ids = []
         for lex in next_lex:
+            # Get the token set for each terminal symbol
             token_set = terminals_map[lex]
             for t in token_set:
+                # Add valid token IDs to the list
                 valid_next_ids.append(t[-1])
         
+        # Return a set of valid next token IDs
         return set(valid_next_ids)
+
 
 class JSONConstraint(GrammarConstraint): 
 
+    # Construct a filter set for valid tokens based on a given expression
     def construct_filter_set(self, expression):
         vocab = self.tokenizer.vocab
         valid_tokens = []
         specials = ['*', '+', ')']
+
+        # Preprocess expression to handle special cases
         if expression[0] in specials:
             expression = f'\{expression}'
         elif len(expression) == 1 and (expression == '[' or  expression == '('):
             expression = f'\{expression}'
 
         try:
+            # Compile the regex pattern and use it to match all valid tokens in the vocabulary
             pattern = re.compile(expression, re.UNICODE)
             for token, id in vocab.items():
                 if pattern.match(token) is not None:
@@ -167,10 +211,11 @@ class JSONConstraint(GrammarConstraint):
         except Exception as e: 
             print(e, expression) 
     
+        # Return a set of valid tokens based on the pattern
         return set(valid_tokens)
     
-
     def parse_grammar(self):
+        # Define JSON grammar
         json_grammar = r"""
         ?start: value
 
@@ -194,6 +239,8 @@ class JSONConstraint(GrammarConstraint):
 
         %ignore WS
         """
+        
+        # Create Lark internal transformer to make parsing faster and more memory efficient 
         class TreeToJson(Transformer):
             @v_args(inline=True)
             def string(self, s):
@@ -212,63 +259,70 @@ class JSONConstraint(GrammarConstraint):
         # Create the JSON parser with Lark, using the LALR algorithm
         self.parser = Lark(json_grammar, parser='lalr',
                         lexer='contextual',
-                        # Using an internal transformer is faster and more memory efficient
                         transformer=TreeToJson())
         terminals = self.parser.terminals
         t_map = {}
         for t in terminals:
             t_map[t.name] = t.pattern.value
+        
+        # Return a map of terminal tokens and their corresponding regex patterns
         return t_map
 
-    def prefix_state(self, prefix_str):
+    def _prefix_state(self, prefix_str):
         valid_next = []
         try:      
+            # Parse the token sequence
             interactive_tree = self.parser.parse_interactive(prefix_str)
             interactive_tree.exhaust_lexer()
+            
+            # Get the valid next states
             valid_next = list(interactive_tree.accepts())
         except Exception as e:
-            print(e)
+            print("Parsing Error: ", e)
+        
+        # Return a list of valid next terminals
         return valid_next
-
+    
     def construct_final_filter_set(self, prefix_ids, terminals_map):
+        # Decode the prefix IDs
         prefix_str = self.tokenizer.batch_decode(prefix_ids, skip_special_tokens=True)[0]
-        print(prefix_str)
-        next_lex = self.prefix_state(prefix_str)
+        
+        # Get valid next terminals for the prefix
+        next_lex = self._prefix_state(prefix_str)
         valid_next_ids = []
         for lex in next_lex:
+            # Get the token set for each terminal symbol
             token_set = terminals_map[lex]
             for t in token_set:
+                # Add valid token IDs to the list
                 valid_next_ids.append(t[-1])
         
+        # Return a set of valid next token IDs
         return set(valid_next_ids)
+
 
 class GrammarLogitsProcessor(LogitsProcessor):
 
     def __init__(self, constraint_class, terminals_map):
-        self.constraint_class = constraint_class
-        self.terminals_map = terminals_map
+        # Initialize the GrammarLogitsProcessor class with a constraint class and terminals map.
+        self.constraint_class = constraint_class 
+        self.terminals_map = terminals_map  
 
     def __call__(self, input_ids, scores):
-
-        print(input_ids)
+        # This method is called for each generation step
+        
+        # Initialize a boolean bias tensor with the same shape as scores
         bias = torch.zeros_like(scores, dtype=torch.bool)
+        
+        # Get the set of valid next token IDs for current step
         valid_next_ids = self.constraint_class.construct_final_filter_set(input_ids, self.terminals_map)
+        
+        # Set the bias to True for valid next token IDs
         for id in valid_next_ids:
             bias[0, id] = True
         
+        # Add the bias to the scores tensor to zero-out invalid next tokens
         scores += bias
+        
+        # Return the modified scores tensor
         return scores
-
-
-prompt = "rand_num = 5"
-
-tokenizer = AutoTokenizer.from_pretrained("gpt2-medium", padding_side='left')
-
-# token_ids = tokenizer(prompt, return_tensors='pt')
-model_kwargs = {
-        'temperature': 1.0,
-        'max_new_tokens': 10
-    }
-model_complete = GrammarCompletion("gpt2-medium", tokenizer)
-result = model_complete.complete(prompt)
-print(result)
