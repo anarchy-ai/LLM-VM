41eb12ec24a9adb0d4e741aa331426ca0c193633
vico <victorch9@gmail.com>
2023-08-31 16:44:02 +0300

    Added test for JSON constraints

--- tests/test_grammar_constraints.py
+++ tests/test_grammar_constraints.py
@@ -2,11 +2,16 @@ from llm_vm.guided_completion import GrammarCompletion
 from transformers import AutoTokenizer
 
 if __name__ == '__main__':
-    
-    prompt = "rand_num = 5"
 
     tokenizer = AutoTokenizer.from_pretrained("gpt2-medium", padding_side='left')
-
     model = GrammarCompletion("gpt2-medium", tokenizer)
+
+    # For python
+    prompt = "rand_num = 5"
     result = model.complete(prompt, temperature=1.0, max_new_tokens=20)
     print(result)
+
+    # for JSON
+    # prompt = "{ \"name\": \"Vik\", \"age\": \"50\","
+    # result = model.complete(prompt, grammar_type="json", temperature=1.0, max_new_tokens=20)
+    # print(result)
