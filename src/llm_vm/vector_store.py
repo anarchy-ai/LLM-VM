10c42cb63f55366269f4245117e30e4a209ee12d
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2023-09-29 13:23:26 -0400

    derp

--- src/llm_vm/vector_store.py
+++ src/llm_vm/vector_store.py
@@ -1,23 +1,23 @@
 
 
+# ultra generic query store interface
 class VectorStore:
-    def __init__(store,query_func):
+    def __init__(self,store,query_func):
         self.store = store # some abstract database you've already loaded and indexed
         self.__query_func = query_func # query_func : (store,search string/vector?) -> List of (document,distance)
-        return None 
 
     def query(snip):
         return self.__query_func(self.store, snip)
 
 
-
-class DocArrayGenericStore(VectorStore):
-    def __init__(store,limit,field):
+# ultra generic doc array query interface 
+class DocArrayGenericStore:
+    def __init__(self,store,limit,field):
         self.store = store  # underlying store
         self.limit = limit   # number of results
         self.field = field   # field in the documents that has the embedding vector 
         # these must all be 
-    def query(snip): # AnyVector -> list of (doc,distance) # distance IS score right?
+    def query(self,snip): # AnyVector -> list of (doc,distance) # distance IS score right?
         if field is not None:
             retrieved_docs, scores = store.find(query=snip,search_field=self.field,limit=self.limit)
         else:
