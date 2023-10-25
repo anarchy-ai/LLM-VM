b3e7dbddf7d701b555c295cce6d729afcd7b2b58
Vik <victorch9@gmail.com>
2023-10-25 18:40:12 +0300

    data synthesis fix up

--- src/llm_vm/completion/data_synthesis.py
+++ src/llm_vm/completion/data_synthesis.py
@@ -10,11 +10,14 @@ import llm_vm.config as conf
 from llm_vm.guided_completion import GenerativeCompletion
 
 class DataSynthesis:
-    def __init__(self, variance, examples_to_generate):
+    def __init__(self, variance, examples_to_generate, seed_examples=10):
         self.variance = variance
+        self.seed_examples = seed_examples
         self.examples_to_generate = examples_to_generate
+        self.optimizer = None
 
-    def data_synthesis(self, optimizer, prompt, response, openai_key=None, completion=None, **kwargs):
+
+    def data_synthesis(self, optimizer, prompt, response, openai_key=None, completion=None, batch_size=10, **kwargs):
         """
         This method generates QA pairs using the larger LLM to be used as training data for fine-tuning the smaller LLM.
 
@@ -35,22 +38,27 @@ class DataSynthesis:
         if os.path.isfile(conf.settings.data_gen_file):
             new_file = open(conf.settings.data_gen_file,"rb")
             return list(pickle.load(new_file))
-        datapoints = []
+        datapoints_list = []
         final_prompt = '{"prompt": "' + prompt +'"  , "response": "' +response+'" }'
-        final_prompt = "Generate {self.examples_to_generate} json similar to the one below and separate each json using a new line. \n" + final_prompt
-        datapoints = self.generate_example(final_prompt, openai_key, completion=completion)
-        if len(datapoints) > self.examples_to_generate:
-            datapoints = datapoints[:self.examples_to_generate]
+        final_prompt = "Generate {batch_size} json similar to the one below and separate each json using a new line. \n" + final_prompt
+        while len(datapoints_list) < self.examples_to_generate:
+            remaining_examples = self.examples_to_generate - len(datapoints_list)
+            if remaining_examples < batch_size:
+                final_prompt = "Generate {remaining_examples} json similar to the one below and separate each json using only 1 new line. \n" + final_prompt                
+            datapoints = self.generate_examples(final_prompt, openai_key, completion=completion)
+            datapoints_list += datapoints
+
         new_file = open(conf.settings.data_gen_file,"wb")
-        pickle.dump(datapoints,new_file)
-        return datapoints
+        pickle.dump(datapoints_list, new_file)
+        return datapoints_list
     
     @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
-    def generate_example(self, final_prompt, openai_key, example_delim="<END>", model="gpt-4", max_tokens=1000, temperature=1, completion=None):
+    def generate_examples(self, final_prompt, openai_key, example_delim="<END>", model="gpt-4", max_tokens=1000, temperature=1, completion=None):
         openai.api_key = openai_key
         cur_prompt = [{'role': "system", 'content': final_prompt}]
-        the_tuple = None
+        tuple_list = []
 
+        # Generate seed prompts and responses using openAI
         response = openai.ChatCompletion.create(messages=cur_prompt, model=model, max_tokens=max_tokens, temperature=temperature)['choices'][0]['message']['content']
         print(response)
         the_data = response.split("\n")
@@ -58,27 +66,48 @@ class DataSynthesis:
         for data in the_data:
             idx = data.index('{')
             json_data = data[idx:]
+            print("JSON_DATA:", json_data)
             json_list.append(json_data)
-        print(json_list) 
-        # the_data = json.loads(response.replace("\n", ""))
-        print(the_data)
+        print(json_list)
+
+        # to use open AI prompts + responses seed_examples must be equal to examples_to_generate
+        if self.examples_to_generate == self.seed_examples:
+            for j in json_list:
+                j_dict = json.loads(j)
+                j_tuple = (j_dict["prompt"], j_dict["response"]+example_delim)
+                tuple_list.append(j_tuple)
+            return tuple_list
+        else:
+            # use seed to generate more prompts
+            pass
+
+        prompt_list = [] 
         if completion is None:
             try:
-                the_data = json.loads(response.replace("\n", ""))
-                prompt = the_data["prompt"]
-                completion_response = self.optimizer.call_big(prompt, max_tokens=max_tokens)
-                print(completion_response)
-                the_tuple = (prompt, completion_response+example_delim)
+                for pair in json_list:
+                    example = json.loads(pair)
+                    prompt = example["prompt"]
+                    prompt_list.append(prompt)
+
+                # Use seed prompts with big model to generate more prompt examples with configured variance                
+                for p in prompt_list:
+                    completion_response = self.optimizer.call_big(p, max_tokens=max_tokens)
+                    print(completion_response)
+                    the_tuple = (p, completion_response+example_delim)
+                    tuple_list.append(the_tuple)
             except Exception as e:
-                raise Exception(f"An error has ocurred: {e}")
-                      
+                raise Exception(f"An error has ocurred: {e}")                     
         else:
             try:
-                the_data = json.loads(response.replace("\n", ""))
-                prompt = the_data["prompt"]
-                completion_response = completion.complete(prompt)
-                the_tuple = (prompt, completion_response+example_delim)
+                for pair in json_list:
+                    example = json.loads(pair)
+                    prompt = example["prompt"]
+                    prompt_list.append(prompt)
+                for p in prompt_list:
+                    completion_response = completion.complete(prompt)
+                    the_tuple = (p, completion_response+example_delim)
+                    tuple_list.append(the_tuple)
             except Exception as e:
                 raise Exception(f"An error has ocurred: ${e}")
-            
-        return the_tuple
+        # use big model prompts + responses to finetune small model     
+        return tuple_list
