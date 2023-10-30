76885c2af3164cf0b5fe0c1236f6490d5a72d5fd
Vik <victorch9@gmail.com>
2023-10-30 12:41:50 +0300

    data synthesis test cleanup

--- tests/test_data_synthesis.py
+++ tests/test_data_synthesis.py
@@ -5,6 +5,7 @@ import openai
 from llm_vm.completion.data_synthesis import DataSynthesis
 from llm_vm.completion.optimize import *
 from llm_vm.client import Client
+import re
 
 client = Client(big_model='gpt', small_model='neo')
 if __name__ == "__main__":
@@ -16,18 +17,21 @@ if __name__ == "__main__":
     print("key:", openai.api_key, file=sys.stderr)
 
 
-    data_synthesizer = DataSynthesis(0.87, 50)
+    data_synthesizer = DataSynthesis(0.87, 10)
 
     # for one-shot prompt
     prompt = "What is the currency in myanmmar?"
-    response = client.complete(prompt=prompt, context = "", openai_key="",temperature = 0.0)["completion"]
-    print(f"Prompt: {prompt} /nResponse: {response}", file=sys.stderr)
+    response = client.complete(prompt=prompt, context = "", openai_key="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",temperature = 0.0)["completion"]
+    f_response = re.sub('\n', '', response)
+    print(f"Prompt: {prompt} \nResponse: {f_response}", file=sys.stderr)
+    data_synthesizer.data_synthesis(client.optimizer, prompt, response, openai_key="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", temperature=0.0)
+
 
     # for k-shot prompt
-    prompt_list = ["What is the currency in Madagascar?", "What is the currency in myanmmar?", "What is the currency in Morocco?"]
-    response_list = []
-    for p in prompt_list:
-        res = client.complete(prompt=p, context = "", openai_key="", temperature = 0.0)
-        response_list.append(res["completion"])
+    # prompt_list = ["What is the currency in Madagascar?", "What is the currency in myanmmar?", "What is the currency in Morocco?"]
+    # response_list = []
+    # for p in prompt_list:
+    #     res = client.complete(prompt=p, context = "", openai_key="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", temperature = 0.0)
+    #     response_list.append(res["completion"])
 
-    data_synthesizer.data_synthesis(client.optimizer, prompt_list, response_list,openai_key="", temperature=0.0)
+    # data_synthesizer.data_synthesis(client.optimizer, prompt_list, response_list, openai_key="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", temperature=0.0)
