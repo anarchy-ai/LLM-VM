ef0100ed0d2c51d6a5f16e929f1056380d6f2079
Vik <victorch9@gmail.com>
2023-10-30 16:05:42 +0300

    remove api keys

--- tests/test_data_synthesis.py
+++ tests/test_data_synthesis.py
@@ -21,17 +21,17 @@ if __name__ == "__main__":
 
     # for one-shot prompt
     prompt = "What is the currency in myanmmar?"
-    response = client.complete(prompt=prompt, context = "", openai_key="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",temperature = 0.0)["completion"]
+    response = client.complete(prompt=prompt, context = "", openai_key="", temperature = 0.0)["completion"]
     f_response = re.sub('\n', '', response)
     print(f"Prompt: {prompt} \nResponse: {f_response}", file=sys.stderr)
-    data_synthesizer.data_synthesis(client.optimizer, prompt, response, openai_key="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", temperature=0.0)
-
+    lst = data_synthesizer.data_synthesis(client.optimizer, prompt, response, openai_key="", temperature=0.0)
+    print("SYNTHESIS COMPLETE:", lst)
 
     # for k-shot prompt
     # prompt_list = ["What is the currency in Madagascar?", "What is the currency in myanmmar?", "What is the currency in Morocco?"]
     # response_list = []
     # for p in prompt_list:
-    #     res = client.complete(prompt=p, context = "", openai_key="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", temperature = 0.0)
+    #     res = client.complete(prompt=p, context = "", openai_key="", temperature = 0.0)
     #     response_list.append(res["completion"])
 
-    # data_synthesizer.data_synthesis(client.optimizer, prompt_list, response_list, openai_key="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", temperature=0.0)
+    # data_synthesizer.data_synthesis(client.optimizer, prompt_list, response_list, openai_key="", temperature=0.0)
