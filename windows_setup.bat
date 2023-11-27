115e2e988715b02043bd2a145daba1cb1c19b461
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-11-27 19:17:03 +0000

    feat: Update virtual environment error messages
    Updated error messages for creating and activating virtual environments.

--- windows_setup.bat
+++ windows_setup.bat
@@ -6,12 +6,18 @@ echo Running setup...
 :: Step 1: Open command prompt in administrator mode
 powershell Start-Process cmd.exe -Verb RunAs -ArgumentList "/k CD %CD% & color 0a"
 
+:: Wait for user to acknowledge the admin prompt
+timeout /t 4 /nobreak >nul
+
 :: Step 2: Check winget version
 winget --version
 if %errorlevel% neq 0 (
     echo Please update your Windows and then re-run the setup.
     goto :eof
 )
+
+:: Wait for a moment before proceeding
+timeout /t 1 /nobreak >nul
 
 :: Step 3: Check Python version and install if not found
 python --version
@@ -24,19 +30,28 @@ if %errorlevel% neq 0 (
     )
 )
 
+:: Wait for a moment before proceeding
+timeout /t 3 /nobreak >nul
+
 :: Step 4: Create a virtual environment
 python -m venv anarchyai
 if %errorlevel% neq 0 (
-    echo Error creating virtual environment. Setup aborted.
+    echo Error creating the virtual environment. Setup aborted.
     goto :eof
 )
+
+:: Wait for a moment before proceeding
+timeout /t 2 /nobreak >nul
 
 :: Step 5: Activate the virtual environment
 anarchyai\Scripts\activate
 if %errorlevel% neq 0 (
-    echo Error activating virtual environment. Setup aborted.
+    echo Error activating the virtual environment. Setup aborted.
     goto :eof
 )
+
+:: Wait for a moment before proceeding
+timeout /t 2 /nobreak >nul
 
 :: Step 6: Install dependencies in development mode
 python -m pip install -e ."[dev]"
