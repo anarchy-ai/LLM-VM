830516699eb0aff3275c964f52c73f41bda2d262
Saisri Vishwanath <saisrivishwanath@Saisris-MacBook-Pro.local>
2023-12-04 01:01:09 -0500

    adding comments

--- examples/quickstart_guided.py
+++ examples/quickstart_guided.py
@@ -11,10 +11,10 @@ client = Client(big_model='pythia', small_model='pythia')
 # response = client.complete(prompt = 'Is 1+1=10000?',
 #                            context='', openai_key=settings.openai_api_key, finetune = False, data_synthesis = False, regex=r"\s*([Yy]es|[Nn]o|[Nn]ever|[Aa]lways)")
 # print(response)
-response = client.complete(prompt = 'Did MJ win 6 titles with the Bulls',
+#response = client.complete(prompt = 'Did MJ win 6 titles with the Bulls',
                          context='', openai_key=settings.openai_api_key, finetune = False, data_synthesis = False,choices=["Hell yeah dude that's correct","No way, that's hella false"])
 # print(response)
-#response = client.complete(prompt = 'What does 1+1 equal?',
-#                           context='',  openai_key=settings.openai_api_key, finetune = True, data_synthesis = True, type="integer")
+response = client.complete(prompt = 'What does 1+1 equal?',
+                           context='',  openai_key=settings.openai_api_key, finetune = True, data_synthesis = True, type="integer")
 print(response)
 
